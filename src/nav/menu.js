b292ff95126fb196a687469fff36a58df8481ae7
mairadavaa <114126275+mairadavaa@users.noreply.github.com>
2022-09-23 11:31:36 +0800

    Update menu.js

--- src/nav/menu.js
+++ src/nav/menu.js
@@ -1,23 +1,7 @@
 import React from 'react';
 
 import { Box } from '@mui/material'
-// import { collection, serverTimestamp} from "@firebase/firestore";
-// import { initializeApp } from "firebase/app";
 
-// import {getFirestore, addDoc } from "firebase/firestore";
-// const firebaseConfig = {
-//   apiKey: "AIzaSyDjuACeDQDzv6LLfqwuICd_cvMEZKURikc",
-//   authDomain: "testmail-4f58b.firebaseapp.com",
-//   projectId: "testmail-4f58b",
-//   storageBucket: "testmail-4f58b.appspot.com",
-//   messagingSenderId: "638641329840",
-//   appId: "1:638641329840:web:2f889baae8aae0a7a84d75",
-//   measurementId: "G-18TK9N11Z3"
-// };
-
-// // Initialize Firebase
-// const app = initializeApp(firebaseConfig);
-// const db = getFirestore(app);
 
 const Menu = () => {
 //   const [list, setList] =useState()

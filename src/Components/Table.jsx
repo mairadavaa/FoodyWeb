993e67018fbc0be0c442962c3e53e05618ad230d
davaamaira <davaamaira@gmail.com>
2022-09-26 11:03:10 +0800

    syyliin5

--- src/Components/Table.jsx
+++ src/Components/Table.jsx
@@ -7,8 +7,7 @@ import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
-import Cart from "../Components/Cart"
-
+import Cart from "../Components/Cart";
 
 export const CollapsibleTable = (text) => {
   let orderArr = [];
@@ -28,92 +27,75 @@ export const CollapsibleTable = (text) => {
     if (el.status === "Алдаатай") {
       mistakenArr.push(el);
     }
-    return
+    return "";
   });
 
   return (
     <Box>
       <TableContainer component={Paper}>
-        <Table
-          style={style.table}
-        >
+        <Table style={style.table}>
           <TableHead>
             <TableRow>
               <TableCell>Захиалсан</TableCell>
             </TableRow>
           </TableHead>
-          <TableBody
-            style={ style.tabletop}
-          >
-            <Cart orders={orderArr}/>
+          <TableBody style={style.tabletop}>
+            <Cart orders={orderArr} />
           </TableBody>
         </Table>
       </TableContainer>
       <TableContainer component={Paper} id="packed">
-        <Table
-          style={style.table}
-        >
+        <Table style={style.table}>
           <TableHead>
             <TableRow>
               <TableCell>Савалсан</TableCell>
             </TableRow>
           </TableHead>
-          <TableBody
-            style={style.tabletop}
-          >
-
-          <Cart orders={packedArr} key ={packedArr.index}/>
+          <TableBody style={style.tabletop}>
+            <Cart orders={packedArr} key={packedArr.index} />
           </TableBody>
         </Table>
       </TableContainer>
       <TableContainer component={Paper} id="delivered">
-        <Table
-          style={style.table}
-        >
+        <Table style={style.table}>
           <TableHead>
             <TableRow>
               <TableCell>Хүргэгдсэн</TableCell>
             </TableRow>
           </TableHead>
-          <TableBody
-            style={style.tabletop}
-          >
-            <Cart orders={deliveredArr}/>
+          <TableBody style={style.tabletop}>
+            <Cart orders={deliveredArr} />
           </TableBody>
         </Table>
       </TableContainer>
       <TableContainer component={Paper} id="mistaken">
-        <Table
-          style={style.table}
-        >
+        <Table style={style.table}>
           <TableHead>
             <TableRow>
               <TableCell>Алдаатай</TableCell>
             </TableRow>
           </TableHead>
-          <TableBody
-             style={style.tabletop}
-          >
-            <Cart orders={mistakenArr}/>
+          <TableBody style={style.tabletop}>
+            <Cart orders={mistakenArr} />
           </TableBody>
         </Table>
       </TableContainer>
     </Box>
   );
 };
-const style ={
-    tabletop:{
-        height: "40vh",
-        overflow: "scroll",
-        display: "flex",
-        flexDirection: "column",
-        width: "90%",
-    },
-    table:{
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        width: "282px",
-        height: "540px"
-    }
-}
+const style = {
+  tabletop: {
+    height: "40vh",
+    overflow: "scroll",
+    display: "flex",
+    flexDirection: "column",
+    width: "90%",
+  },
+  table: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    width: "282px",
+    height: "540px",
+  },
+};
